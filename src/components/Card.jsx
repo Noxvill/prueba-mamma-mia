7615068cedd462670e68a815adcc613e52788e3c
Noxvill <gmo_mmo@live.cl>
2024-03-12 01:01:22 -0300

    cambios lógicas y diseño

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,53 +1,45 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { PizzasContext } from "../context/PizzaProv";
-// import { formatNumber } from "../helpers/formatNumber";
+
 
 const Card = () => {
-  // const [total, setTotal] = useState(0);
+
   const { pizzas, addToCart } = useContext(PizzasContext);
 
   const navigate = useNavigate();
 
   return (
     <>
-      {pizzas.map((pizza) => (
-        <div
-          key={pizza.id}
-          className="col"
-        >
-          <div className="card">
-            <img
-              className="card-img-top"
-              src={pizza.img}
-              alt=""
-            />
+
+    {pizzas.map((pizza) => (
+        <div key={pizza.id} className="col">
+          <div >
+            <img className="card-img-top"
+              src={pizza.img}/>
+
             <div className="card-body">
-              <h4 className="card-title text-capitalize">Pizza {pizza.name}</h4>
-              <hr />
+              <h4>Pizza {pizza.name}</h4>
+           
               <p className="card-text">
-                <b>Ingredientes:</b>
+                <h6>Ingredientes:</h6>
               </p>
 
-              <ul>
                 {pizza.ingredients.map((ingredient, i) => (
                   <li key={i}> {ingredient}</li>
                 ))}
-              </ul>
+             
             </div>
 
             <h2>
               {"$"}{pizza.price}</h2>
 
-            <div className="d-flex justify-content-around mb-4">
+            <div >
               <button
-                to={`pizza/${pizza.id}`}
-                className="btn btn-info text-white"
-                onClick={() => navigate(`/pizzas/${pizza.id}`)}
-              >
+                to={`pizza/${pizza.id}`} className="btn btn-info text-white" onClick={() => navigate(`/pizzas/${pizza.id}`)}>
                 Ver Más
               </button>
-
+              &nbsp;&nbsp;&nbsp;
               <button
                 className="btn btn-danger"
                 onClick={() => addToCart(pizza)}
--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -4,34 +4,41 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import './Menubar.css'
 import { Link } from "react-router-dom";
-
+import { useContext } from "react";
+import { PizzasContext } from "../context/PizzaProv";
 
 export default function Menubar() {
 const setActiveClass = ({ isActive }) => (isActive ? "active" : undefined);
-
+const { carrito } = useContext(PizzasContext);
+const total = carrito.reduce((a, { count, price }) => a + price * count, 0);
 
 
 return (
 <div>
 <Navbar  expand="lg" fixed="top" style={{ height: '80px', backgroundColor:'rgb(37, 150, 190)'}}>
         <Container>
+
+        <Link
+            to="/">
         <Navbar.Brand>
           <img
-            src="/pokemon-go.svg"
+            src="/pizza-icon.webp"
             width="30"
             height="30"
             className="d-inline-block align-top"
-            alt="Pokemon Go Logo"
-          />Pizzería Mamma mía!
+            alt="Pizzas mamma mía"
+          />&nbsp;Pizzería Mamma mía!
         </Navbar.Brand>
-  
+        </Link>
 
         <Nav className="me-auto" style={{ marginLeft: '80%'}}>
       
-        <NavLink className={`text-white ${setActiveClass}`} to="/Home" style={{ marginRight: '10px' }} >Home</NavLink>
+        {/* <NavLink className={`text-white ${setActiveClass}`} to="/Home" style={{ marginRight: '10px' }} >Home</NavLink>
         <NavLink className={`text-white ${setActiveClass}`} to="/Pokemones">Pokemones</NavLink>
-         
-      
+          */}
+      <h4 className="mb-0">
+          {""} Total: ${(total)}
+            </h4>
         </Nav>
         </Container>
       </Navbar>
